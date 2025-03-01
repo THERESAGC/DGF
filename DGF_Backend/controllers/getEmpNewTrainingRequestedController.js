9a895eb792727bc14d7da9178ede9cba9ec2cebc
Sanket Tidke <Sanket.Tidke@harbingergroup.com>
2025-03-01 10:38:53 +0530

    fixed learners api

--- DGF_Backend/controllers/getEmpNewTrainingRequestedController.js
+++ DGF_Backend/controllers/getEmpNewTrainingRequestedController.js
@@ -1,15 +1,16 @@
 const { getEmpNewTrainingRequestedByRequestId } = require('../services/getEmpNewTrainingRequestedService');
 
+// Controller to handle getting data from emp_newtrainingrequested based on requestid
 const getEmpNewTrainingRequested = async (req, res) => {
-    const { requestid } = req.params;
+    const { requestid } = req.query;
 
     if (!requestid) {
         return res.status(400).json({ error: 'Request ID is required' });
     }
 
     try {
-        const results = await getEmpNewTrainingRequestedByRequestId(requestid);
-        res.status(200).json(results);
+        const data = await getEmpNewTrainingRequestedByRequestId(requestid);
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: 'An error occurred while retrieving data', details: error.message });
     }
